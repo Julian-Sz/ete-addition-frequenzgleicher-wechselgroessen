912549c7f31c87c5e8b93897a8eaf6dfd9b9ac66
Julian Szigethy <julian.szigethy@gmail.com>
2021-10-10 21:05:58 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useReducer } from "react";
+import "./App.css";
+import "./index.css";
+import InputPrompt from "./components/inputPrompt";
+
+const ACTIONS = {
+  ADD_ZEIGER: "add-zeiger",
+  RESET: "reset",
+};
 
 function App() {
+  const reducer = (prev, action) => {
+    switch (action.type) {
+      default:
+        return prev;
+    }
+  };
+  const store = useReducer(reducer);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Addition frequenzgleicher Wechselgrößen - Visualisierer</h1>
       </header>
+      <main>
+        <InputPrompt />
+      </main>
+      <footer>Copyright 2021 by Julian Szigethy</footer>
     </div>
   );
 }
